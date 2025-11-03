638aa17a7485d4122cb1fcb346165f9b70addd21
Daniel S. Ramos <danikzero@hotmail.com>
2025-11-03 09:54:37 -0300

    Dados das tecnologias usadas

---
+++ src/data/technologies.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    id: 1,
+    title: 'Game Engines',
+    description: 'Unity, Unreal Engine e tecnologias próprias para diferentes plataformas',
+    icon: [
+      'bx bx-chip'
+    ]
+  },
+  {
+    id: 2,
+    title: 'Desenvolvimento Mobile',
+    description: 'Vue e tecnologias híbridas crossplatform para iOS e Android',
+    icon: [
+      'bx bx-devices'
+    ],
+
+  },
+  {
+    id: 3,
+    title: 'Analytics e Performance',
+    description: 'Ferramentas avançadas de análise e otimização de jogos',
+    icon: [
+      'bx bx-line-chart-down'
+    ]
+  }
+]
