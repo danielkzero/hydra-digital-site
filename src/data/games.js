a1a5d2accda1d5d9c34297c690a98ee72dd99b06
Daniel S. Ramos <danikzero@hotmail.com>
2025-11-03 09:54:24 -0300

    Dados dos jogos

---
+++ src/data/games.js
@@ -0,0 +1,33 @@
+import NhacMania from '@/assets/images/NhacMania.jpg';
+import ZigZagTap from '@/assets/images/ZigZagTap.png';
+import MarcioBraulio from '@/assets/images/MarcioBraulio1.jpeg';
+
+export default [
+  {
+    id: 1,
+    title: 'Nhac Mania',
+    description: 'Combine vegetais e divirta-se! Ajude o nosso porquinho capturando os vegetais, itens e cumprindo missões e encontrando novos itens.',
+    tags: ['Puzzle', 'Casual'],
+    rating: 5,
+    downloads: '10',
+    coverImage: NhacMania
+  },
+  {
+    id: 2,
+    title: 'Zig Zag Tap',
+    description: 'Pegue os pontos iguais e desvie dos diferentes, com apenas um toque mude a direção e desvie dos obstáculos. Se desafie e quebre seus recordes!', 
+    tags: ['Ação', 'Casual'],
+    rating: 5,
+    downloads: '10',
+    coverImage: ZigZagTap
+  },
+  {
+    id: 3,
+    title: 'Hyper Márcio Bráulio Adventure',
+    description: 'Márcio Bráulio vê sua cidade ser tomada por ratos gigantes enquanto pessoas somem misteriosamente. Corra, lute e descubra o que está acontecendo!',
+    tags: ['Ação', 'Aventura', 'Plataforma'],
+    rating: 5,
+    downloads: '10',
+    coverImage: MarcioBraulio
+  },
+];
