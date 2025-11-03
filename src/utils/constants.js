e0cd1de9485f361d795e0fde811caf957d209540
Daniel S. Ramos <danikzero@hotmail.com>
2025-11-03 09:55:13 -0300

    Constantes globais

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const NAVBAR_HEIGHT = 80;
+export const SCROLL_THRESHOLD = 50;
+
+export const SECTION_IDS = {
+  TOP: 'top',
+  APPS: 'apps',
+  GAMES: 'games',
+  CONTACT: 'contact'
+};
+
+export const ANIMATION_DURATIONS = {
+  FADE: '0.3s',
+  CONTENT_FADE: '3s',
+  ZOOM_FADE: '2.5s'
+};
